eda91d0de34098115b07441176a022bda3f9fa58
dana sandman <sandmandana1@gmail.com>
2021-09-19 00:11:23 +0300

    scss

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -24,7 +24,9 @@ export function Home() {
       <h2 className="main-title">טאץ׳ והחלפתם פרטים</h2>
       <img src={TapTouchImg} className="main-img" alt="main-img" />
       <h4 className="paragraph">?אז מה תרצו שהספלינג שלכם יעשה</h4>
+      <div className="media-details">
       <MediaDetails />
+      </div>
       <h4 className="paragraph"> !עלות התג 49 ש״ח בלבד כולל משלוח</h4>
       <Button className={classes.button} variant="contained" disableElevation>
         <Link to="/order"> 〱 הזמינו עכשיו</Link>
